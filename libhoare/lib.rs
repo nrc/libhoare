804956a3c3f4e1021b2340ff773012f1e5a6487f
Nick Cameron <ncameron@mozilla.com>
2015-08-09 17:48:56 +1200

    Use cfg! for debug assertions

--- libhoare/lib.rs
+++ libhoare/lib.rs
@@ -283,8 +283,7 @@ fn debug_invariant(cx: &mut ExtCtxt,
 fn if_debug<F>(cx: &mut ExtCtxt, f: F, item: Annotatable) -> Annotatable
     where F: Fn(&mut ExtCtxt) -> Annotatable
 {
-    if !cx.cfg().iter().any(
-        |item| item.node == ast::MetaWord(token::get_name(token::intern("ndebug")))) {
+    if cfg!(debug_assertions) {
         f(cx)
     } else {
         item
